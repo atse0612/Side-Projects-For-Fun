6aa7831efe4ac18a44e39d473ad06a2b993540cc
A. Tse <atse0612@users.noreply.github.com>
2018-11-11 21:40:00 -0800

    Add files via upload

--- NYSE/NYSE.R
+++ NYSE/NYSE.R
@@ -50,3 +50,35 @@ ohlc <- RL[c("date","open","high","low","close","volume")] #OHLC dataframe as th
 head(ohlc)
 
 
+## Picking FB
+
+
+FB <- subset(nyse,symbol=="FB") #extracting only Ralph Lauren
+
+FB[1:3,]
+close <- FB$close #creating vector called close
+close[1:3] 
+plot(close, type="l",xlab = "days",ylab = "Closing Price",main="Plotting Line Chart of FB Stock") #plotting a line chart
+returns <- (close[1:(length(close)-1)]-close[2:length(close)])/close[2:length(close)] #calculating arthmatic daily return
+plot(returns,type = "l",xlab = "RL",main="Plotting daily returns of FB Stock") #plotting daily return chart
+macd <- MACD(close,nFast=12,nSlow=26,nSig = 9,maType = SMA,percent = FALSE) #MACD calculation
+ohlc <- FB[c("date","open","high","low","close","volume")] #OHLC dataframe as the excel data cant be used to create a candlestick chart
+head(ohlc)
+
+
+## Picking PG
+
+
+PG <- subset(nyse,symbol=="PG") #extracting only PG
+
+PG[1:3,]
+close <- PG$close #creating vector called close
+close[1:3] 
+plot(close, type="l",xlab = "days",ylab = "Closing Price",main="Plotting Line Chart of PG Stock") #plotting a line chart
+returns <- (close[1:(length(close)-1)]-close[2:length(close)])/close[2:length(close)] #calculating arthmatic daily return
+plot(returns,type = "l",xlab = "RL",main="Plotting daily returns of PG Stock") #plotting daily return chart
+macd <- MACD(close,nFast=12,nSlow=26,nSig = 9,maType = SMA,percent = FALSE) #MACD calculation
+ohlc <- PG[c("date","open","high","low","close","volume")] #OHLC dataframe as the excel data cant be used to create a candlestick chart
+head(ohlc)
+
+
